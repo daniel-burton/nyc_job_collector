6dc819ec2f965a9b13f72481cb696ad05b148862
daniel-burton <daniel.r.burton@gmail.com>
2018-10-22 22:08:13 -0400

    working on testing

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# NYC Job Collector: a simple script to collect all currently open NYC government jobs and list them on a Flask site
+# NYC Job Collector: 
+## A simple script to collect all currently open NYC government jobs and list them on a Flask site
+
+Not yet fully working. Test cases progressing but the full project is
+forthcoming.
--- attempt.py
+++ attempt.py
@@ -1,32 +1,39 @@
 import re
+import time
 import requests
 import csv
 
 all_sites = ['cas', 'cig', 'fap', 'itt', 'poa', 'soc', 'cbs', 'eap', 'hlt',
              'leg', 'mop', 'psi']
+# the list of job categories
 
 root = ('https://a127-jobs.nyc.gov/psc/nycjobs/EMPLOYEE/HRMS/c'
         '/HRS_HRAM.HRS_APP_SCHJOB.GBL?FOCUS=Applicant&category=')
+# root is the basic address for getting the HTML of the job entries, usually
+# fetched by the javascript on the NYC jobs explorer site. Add the category at
+# the end to get full address.
 
-full_sites = [root + site for site in all_sites]
+address = ("https://a127-jobs.nyc.gov/psc/nycjobs/EMPLOYEE/HRMS/c"
+          "/HRS_HRAM.HRS_APP_SCHJOB.GBL?Page=HRS_APP_JBPST&Action"
+          "=U&FOCUS=Applicant&SiteId=1&JobOpeningId={}&PostingSeq=1")
+# address is for generating the permalinks using job id
 
-all_jobs = [] #will contain list of lists of tuples, each tuple is job details
+entry = re.compile(r"Display details of (.+) -"
+                    "(\d\d\d\d\d\d).+<b>Department:</b>(.+)\s+ \|")
+# entry = re.compile(r"Display details of (.+) - (\d\d\d\d\d\d)")
+# the job description always includes this tag in the HTML
 
-for site in full_sites:
-    all_jobs.append(requests.get(site))
-
-
-address = "https://a127-jobs.nyc.gov/psc/nycjobs/EMPLOYEE/HRMS/c/HRS_HRAM.HRS_APP_SCHJOB.GBL?Page=HRS_APP_JBPST&Action=U&FOCUS=Applicant&SiteId=1&JobOpeningId={}&PostingSeq=1"
-
-entry = re.compile(r"Display details of (.+) - (\d\d\d\d\d\d)")
-
-for this_job in all_jobs:
-    jobs.append(re.findall(entry, thisjob.text))
-
-with open('./jobs.csv',"w") as out:
-    outwrite = csv.writer(out)
-    for title, number in jobs:
-        location = title.find('-')
-        outwrite.writerow(number, title[:location],address.format(number))
-
-#print(jobs)
+for site in all_sites:
+    """this iterates through the categories, fetches the HTML, uses the regex
+    to find the jobs, writes them to the csv."""
+    full_address = root + site
+    full_page = requests.get(full_address)
+    jobs = re.findall(entry, full_page.text)
+    with open('./jobs.csv','w') as out:
+        outwrite = csv.writer(out)
+        for title, number, department in jobs:
+            location = title.find('-')
+            permalink = address.format(number)
+            outwrite.writerow([site, department, number, title[:location],
+                               permalink])
+    time.sleep(5)

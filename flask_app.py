3e99c76de35e8f6d4af46f39338d963f5cf87ffd
daniel-burton <daniel.r.burton@gmail.com>
2018-11-10 16:13:00 -0500

    get

--- flask_app.py
+++ flask_app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def jobs_route():
     with open("jobs.json","r") as job_file:
         jobs_data = json.loads(job_file.read())

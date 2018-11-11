c1b3ae25f657072831a647dfb34859c27b89c368
daniel-burton <daniel.r.burton@gmail.com>
2018-11-10 19:22:40 -0500

    show last updated

--- flask_app.py
+++ flask_app.py
@@ -11,9 +11,12 @@ app = Flask(__name__)
 def jobs_route():
     with open("jobs.json","r") as job_file:
         jobs_data = json.loads(job_file.read())
+    with open("job_log.txt","r") as log_file:
+        log = log_file.readline()
     return render_template(
         "jobs.html",
         jobs = jobs_data,
+        updated = log
         )
 
 if __name__ == '__main__':
